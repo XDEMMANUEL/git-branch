9707c49e8baaa852dcf0f4c5c01f50c4436ce7cd
XDEMMANUEL <eanichebe85@gmail.com>
2024-08-14 01:43:09 -0700

    features

---
+++ features.js
@@ -0,0 +1,8 @@
+const figure1 = 88;
+const figure2 = 33;
+
+if(figure1 > figure2) {
+    console.log(figure1 + ' is greater than ' + figure2);
+} else{
+    console.log(figure2 + ' is greater than ' + figure1);
+}
